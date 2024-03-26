c785b9fd55537cb9e1ca6a646b78a880566cef7f
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-26 12:27:01 +0300

    added new unit test to TestUtil.java and reached %100 coverage.

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,4 +12,41 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testArgsLengthEquals1(){
+    int[] args = new int[1];
+    assertFalse(c.compute(args));
+  }
+
+  @Test
+  public void testArgsLengthIsEven(){
+    int[] args = new int[10];
+    assertFalse(c.compute(args));
+  }
+
+  @Test
+  public void testException(){
+    int[] args = {1,2,0};
+    boolean exception = false;
+    try{
+      c.compute(args);
+    } catch(RuntimeException e){
+        exception = true;
+    }
+    assertTrue(exception);
+  }
+
+  @Test
+  public void testSumModArgs(){
+    int[] args = {2,2,2,2,2};
+    assertTrue(c.compute(args));
+  }
+
+  @Test
+  public void testSumNotModArgs(){
+    int[] args = {2,2,2,2,3};
+    assertFalse(c.compute(args));
+  }
+
 }
