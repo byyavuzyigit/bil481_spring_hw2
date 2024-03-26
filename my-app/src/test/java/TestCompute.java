02f2e9b0a905f175c4db73fb6d725af95d04e0c0
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-26 16:33:50 +0300

    added new unit tests to TestCompute.java and reached %100 coverage.

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -31,4 +31,16 @@ public class TestCompute {
     c = new Compute(mq);
     assertEquals(c.countNumberOfOccurrences(""),0);
   }
+
+  @Test
+  public void testCounter(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("a")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("a");
+    when(mq.getAt(1)).thenReturn("a");
+    when(mq.getAt(2)).thenReturn("b");
+    c = new Compute(mq);
+    assertEquals(c.countNumberOfOccurrences("a"),2);
+  }
 }
